a8aee91dc56f0c8eeb4f3bdb2a7ee03f62622668
HenriChabrand <HenriChabrand@users.noreply.github.com>
2016-09-19 16:01:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const synaptic = require('synaptic');
+
+
+/*const synaptic = require('synaptic');
 
 var Layer = synaptic.Layer,
     Network = synaptic.Network,
     Architect = synaptic.Architect;
     
-    
+ */
+ 
+ 
+var algorithmia = require("algorithmia");
+
+
+ 
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -17,8 +25,10 @@ app.use(bodyParser.json());
 // POST http://heroku-root/api/v1/
 // parameters sent with 
 app.post('/api/v1', function(req, res) {
-	
-	var json_step = req.body
+
+
+/*
+var json_step = req.body;
   
 var myLSTM = new Architect.LSTM(3,6,1);
 
@@ -30,9 +40,19 @@ for (var i = 0; i < 200000; i++)
     myLSTM.activate([i/20000,(i+1)/20000,(i+2)/20000]);
     myLSTM.propagate(learningRate, [(i+3)/20000]);
 
-}
+} 
+
+res.send(myLSTM.activate([1234/20000,1235/20000,1236/20000]));
+  */
   
-  res.send(myLSTM.activate([1234/20000,1235/20000,1236/20000]));
+  var input = [[0,1,2,3,4,5,6,7,8,9,10],5,2];
+algorithmia.client("sim9VjrH8Tq+hYi9cPvHEr6ACt71")
+           .algo("algo://TimeSeries/Forecast/0.2.0")
+           .pipe(input)
+           .then(function(response) {
+             res.send(response.get());
+           });
+ 
 });
 
 // start the server
